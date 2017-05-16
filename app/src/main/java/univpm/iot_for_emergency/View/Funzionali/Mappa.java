e223cd0ba2bc4c3c2235cbb7507f219ae4795cae
Giuseppe Romani <gromani14@gmail.com>
2017-05-16 17:39:23 +0200

    ciao

--- app/src/main/java/univpm/iot_for_emergency/View/Funzionali/Mappa.java
+++ app/src/main/java/univpm/iot_for_emergency/View/Funzionali/Mappa.java
@@ -6,9 +6,14 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.Rect;
+import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import univpm.iot_for_emergency.R;
 
@@ -24,13 +29,43 @@ public class Mappa  extends ImageView{
     }
 
 
-    public void init(){
+    public void init(Toolbar toolbar){
         Bitmap bMap = BitmapFactory.decodeResource(getResources(), R.drawable.q145_color);
         Bitmap mutableBitmap = bMap.copy(Bitmap.Config.ARGB_8888, true);
         Bitmap bMap1 = BitmapFactory.decodeResource(getResources(),R.drawable.sensortag);
         Bitmap mutableBitmap1 = bMap1.copy(Bitmap.Config.ARGB_8888, true);
         mutableBitmap=overlay(mutableBitmap,mutableBitmap1);
+        mutableBitmap=overlay(mutableBitmap,mutableBitmap1);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.FILL_PARENT, RelativeLayout.LayoutParams.FILL_PARENT);
+        params.addRule(RelativeLayout.BELOW, toolbar.getId());
+        int dpValue = 43; // margin in dips
+        float d = this.getResources().getDisplayMetrics().density;
+        int margin = (int)(dpValue * d); // margin in pixels
+        Log.e("dp ", String.valueOf(margin));
+        params.setMargins(0,margin,0,0);
+        this.setLayoutParams(params);
         this.setImageBitmap(mutableBitmap);
+        this.setOnTouchListener(new OnTouchListener() {
+
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                float eventX = event.getX();
+                float eventY = event.getY();
+
+                switch (event.getAction())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                        String posizione="("+Math.round(eventX)+","+Math.round(eventY)+")";
+                        Log.e("dp ", "Posizione "+posizione);
+
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        return true;
+                }
+                return false;            }
+        });
     }
 
     private Bitmap overlay(Bitmap bmp1, Bitmap bmp2) {
